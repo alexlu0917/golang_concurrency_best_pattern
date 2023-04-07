e4dce2253b366e795afcc4eb5904c8bddfe95387
joeli <joelideveloper@gmail.com>
2023-04-07 11:53:46 -0500

    create generator function

--- updated_consumer_error_handle_pattern/main.go
+++ updated_consumer_error_handle_pattern/main.go
@@ -2,6 +2,29 @@ package update_consumer_error_handle_pattern
 
 import "fmt"
 
+type Result struct {
+	data int
+	err  error
+}
+
 func StartPoint() {
 	fmt.Println("Updated Consumer error handle pattern")
+	// input := []int{1, 2, 3, 4}
+	// resultCh := make(chan Result)
+
+	// inputCh := generator(input)
+}
+
+func generator(input []int) chan int {
+	inputCh := make(chan int)
+
+	go func() {
+		defer close(inputCh)
+
+		for data := range input {
+			inputCh <- data
+		}
+	}()
+
+	return inputCh
 }
