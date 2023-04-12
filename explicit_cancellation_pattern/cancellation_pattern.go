3dd8e00d6ff7dafce5c1409d3559a320a67491e4
alexlu0917 <alexlu0917.dev@gmail.com>
2023-04-12 17:00:31 -0500

    create add func with cancellation
    * create done channel
    * check done channel
    * quit operation if done channel is valid

--- explicit_cancellation_pattern/cancellation_pattern.go
+++ explicit_cancellation_pattern/cancellation_pattern.go
@@ -4,6 +4,37 @@ import "fmt"
 
 func ExplicitCancellationPattern() {
 	fmt.Println("Explicit Cancellation Patter")
+
+	inputArray := []int{1, 2, 3, 4, 5}
+
+	doneCh := make(chan struct{})
+	defer close(doneCh)
+
+	inputCh := generator(inputArray)
+	resultCh := add(doneCh, inputCh)
+
+	for data := range resultCh {
+		fmt.Println(data)
+	}
+}
+
+func add(doneCh chan struct{}, inputCh chan int) chan int {
+	resultCh := make(chan int)
+
+	go func() {
+		defer close(resultCh)
+
+		for data := range inputCh {
+			result := data + 1
+			select {
+			case <-doneCh:
+				return
+			case resultCh <- result:
+			}
+		}
+	}()
+
+	return resultCh
 }
 
 func generator(input []int) chan int {
